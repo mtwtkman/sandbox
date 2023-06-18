7382fc23b6650e50bf19d559239445b298f2eeb6
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-06-18 11:18:33 +0900

    clean

--- haskell/doubling/Doubling.hs
+++ haskell/doubling/Doubling.hs
@@ -6,9 +6,12 @@ import Data.Bits (shiftR, (.&.))
 pow :: Int -> Int -> Int
 pow x n = go x n 1
   where
-    go _ 0 acc = acc
-    go x n acc =
-      go (x * x) (n `shiftR` 1) (acc * (if n .&. 1 == 1 then x else 1))
+    go x n acc
+      | n == 0 = acc
+      | otherwise = go (x * x) (n `shiftR` 1) (acc * (if n .&. 1 == 1 then x else 1))
 
 main :: IO ()
-main = print $ pow 100 8
+main = do
+  print $ pow 2 3
+  print $ pow 7 2
+  print $ pow 7 0
