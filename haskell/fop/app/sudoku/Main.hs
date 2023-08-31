14108a89d43d94db9a681d3c6114968716533bfb
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-08-31 18:14:32 +0900

    sdk

--- haskell/fop/app/sudoku/Main.hs
+++ haskell/fop/app/sudoku/Main.hs
@@ -3,7 +3,11 @@
 
 module Main where
 
+import Control.Applicative (Alternative (..))
 import Data.Bifunctor (first)
+import Data.Char (digitToInt)
+import Data.List (intersperse)
+import Prelude hiding (all)
 
 data Triple a = Tr a a a
   deriving (Show)
@@ -40,6 +44,15 @@ type Zone = Triple :. Triple
 
 type Board = Zone :. Zone
 
+rows :: Board a -> Board a
+rows = id
+
+cols :: Board a -> Board a
+cols = Comp . sequenceA . comp
+
+boxs :: Board a -> Board a
+boxs = Comp . Comp . fmap ((fmap Comp . sequenceA) . fmap comp) . comp . comp
+
 newtype Parse x = Parser {parse :: String -> [(x, String)]} deriving (Semigroup, Monoid)
 
 instance Functor Parse where
@@ -48,6 +61,100 @@ instance Functor Parse where
 instance Applicative Parse where
   pure x = Parser $ \s -> [(x, s)]
   Parser f <*> Parser x = Parser $ \s -> [(s' s'', r') | (s', r) <- f s, (s'', r') <- x r]
+
+instance Alternative Parse where
+  empty = mempty
+  (<|>) = mappend
+
+ch :: (Char -> Bool) -> Parse Char
+ch p = Parser f
+  where
+    f (x : xs) | p x = [(x, xs)]
+    f _ = []
+
+whitespace :: Parse Char
+whitespace = ch (== ' ')
+
+digit :: Parse Char
+digit = ch $ flip elem ['0' .. '9']
+
+then' :: Parse Char -> Parse String -> Parse String
+then' p q = (:) <$> p <*> q
+
+rep :: Parse Char -> Parse String
+rep p = then' p (rep p) <|> pure ""
+
+square :: Parse Int
+square = digitToInt <$> ((\_ x -> x) <$> rep whitespace <*> digit)
+
+board :: Parse (Board Int)
+board = traverse (const square) (pure 0 :: Board Int)
+
+b_ :: String
+b_ =
+  mconcat $
+    intersperse
+      " "
+      [ "0 0 0 0 0 0 6 8 0",
+        "0 0 0 0 7 3 0 0 9",
+        "3 0 9 0 0 0 0 4 5",
+        "4 9 0 0 0 0 0 0 0",
+        "8 0 3 0 5 0 9 0 2",
+        "0 0 0 0 0 0 0 3 6",
+        "9 6 0 0 0 0 3 0 8",
+        "7 0 0 6 8 0 0 0 0",
+        "0 2 8 0 0 0 0 0 0"
+      ]
+
+newtype K a x = K {unK :: a}
+  deriving (Show)
+
+instance (Monoid a) => Functor (K a) where
+  fmap _ (K a) = K a
+
+instance (Monoid a) => Applicative (K a) where
+  pure _ = K mempty
+  K a <*> K b = K $ a <> b
+
+crush :: (Traversable f, Monoid b) => (a -> b) -> f a -> b
+crush f t = unK $ traverse (K . f) t
+
+newtype Any = Any {unAny :: Bool} deriving (Show)
+
+instance Semigroup Any where
+  (Any x) <> (Any y) = Any $ x || y
+
+instance Monoid Any where
+  mempty = Any False
+
+newtype All = All {unAll :: Bool} deriving (Show)
+
+instance Semigroup All where
+  (All x) <> (All y) = All $ x && y
+
+instance Monoid All where
+  mempty = All False
+
+any :: (Traversable t) => (a -> Bool) -> t a -> Bool
+any p = unAny . crush (Any . p)
+
+all :: (Traversable t) => (a -> Bool) -> t a -> Bool
+all p = unAll . crush (All . p)
+
+duplicates :: (Traversable f, Eq a) => f a -> [a]
+duplicates = fmap fst . filter ((> 1) . snd) . foldr count []
+
+count :: (Eq a) => a -> [(a, Int)] -> [(a, Int)]
+count x [] = [(x, 1)]
+count x ((x', c) : xs)
+  | x == x' = (x', c + 1) : xs
+  | otherwise = (x', c) : count x xs
+
+complete :: Board Int -> Bool
+complete = all (`elem` [1 .. 9])
+
+ok :: Board Int -> Bool
+ok t = all (\f -> null $ duplicates $ f t) [rows, cols, boxs]
 
 main :: IO ()
 main = return ()
