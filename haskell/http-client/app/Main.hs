e747bf0ac09f077acf1e7f0891fdff9b621b68c1
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-01-15 20:57:06 +0900

    using conversations.history of slack api

--- haskell/http-client/app/Main.hs
+++ haskell/http-client/app/Main.hs
@@ -1,19 +1,72 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Network.HTTP.Req
+import Data.Aeson (ToJSON, Value)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import Data.Text (Text, unpack)
+import GHC.Generics (Generic)
+import Network.HTTP.Req (
+  Option,
+  POST (POST),
+  ReqBodyJson (ReqBodyJson),
+  Scheme (Https),
+  defaultHttpConfig,
+  header,
+  jsonResponse,
+  req,
+  responseBody,
+  runReq,
+  useHttpsURI,
+ )
+import Text.URI (mkURI)
 
+import Data.Maybe (fromJust)
 import System.Environment (getEnv)
 
-tokenName :: String
-tokenName = "SLACK_API_TOKEN"
+fetchChannelId :: IO String
+fetchChannelId = getEnv "CHANNEL"
 
-fetchToken :: IO String
-fetchToken = getEnv tokenName
+tokenName :: Text
+tokenName = "TOKEN"
 
-apiUrl :: String
+fetchToken :: IO B.ByteString
+fetchToken = do
+  token <- getEnv (unpack tokenName)
+  return (C.pack token)
+
+apiUrl :: Text
 apiUrl = "https://slack.com/api/"
+
+authHeader :: B.ByteString -> Option Https
+authHeader token = header "Authorization" ("Bearer " <> token)
+
+postReq :: (ToJSON a) => B.ByteString -> Text -> a -> IO Value
+postReq token endpoint payload = runReq defaultHttpConfig $ do
+  uri <- mkURI (apiUrl <> endpoint)
+  let (url, options) = fromJust (useHttpsURI uri)
+  r <-
+    req POST url (ReqBodyJson payload) jsonResponse $
+      authHeader token
+        <> options
+  return $ responseBody r
+
+newtype ConversationsHistoryPayload = ConversationsHistoryPayload
+  { channel :: String
+  }
+  deriving (Show, Eq, Generic)
+
+instance ToJSON ConversationsHistoryPayload
+
+postConversationsHistory :: B.ByteString -> ConversationsHistoryPayload -> IO Value
+postConversationsHistory token = postReq token "conversations.history"
 
 main :: IO ()
 main = do
   token <- fetchToken
-  print token
+  channelId <- fetchChannelId
+  resp <- postConversationsHistory token (ConversationsHistoryPayload channelId)
+  print resp
