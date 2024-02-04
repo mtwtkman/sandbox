3f464b5807eca27a53980ac8b5e5e4a21db05916
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-02-04 16:29:20 +0900

    added Lan data type

--- haskell/freer-monad/app/FFree.hs
+++ haskell/freer-monad/app/FFree.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
 
 module FFree where
 
 import Control.Monad ((>=>))
+import Data.Kind (Type)
 
 data FFree f a where
   Pure :: a -> FFree f a
@@ -27,3 +29,12 @@ instance Applicative (FFree f) where
 instance Monad (FFree f) where
   Impure fx k' >>= k = Impure fx (k' >=> k)
   Pure a >>= k = k a
+
+data Lan (g :: Type -> Type) a where
+  FMap :: (x -> a) -> g x -> Lan g a
+
+instance Functor (Lan g) where
+  fmap h (FMap h' gx) = FMap (h . h') gx
+
+data BiFree p a b where
+  Bimap :: (a -> b) -> (c -> d) -> p a c -> BiFree p b d
